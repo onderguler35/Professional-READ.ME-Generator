3c1fd21bccdd5d9b44c45fc5e72e1f8d0fda31e4
Gatsby <115715220+onderguler35@users.noreply.github.com>
2023-01-24 20:23:41 +0000

    applied formating to code

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const fs = require("fs"); // file system management package which we need to write into the local file. It is an inhouse package of node.
 const inquirer = require("inquirer"); // this is the only package dependency we need to recieve input from the user in the command line asking questions and getting the answers in an object format which then we can parse and write to local file.
-const generateMarkdown = require("./utils/generateMarkdown"); //importing a function from another module 
-const questions = require('./utils/questions'); //importing a function from another module 
-
+const generateMarkdown = require("./utils/generateMarkdown"); //importing a function from another module
+const questions = require("./utils/questions"); //importing a function from another module
 
 // function to write README file
 // it will use the data coming from the generateMarkdown function
 function writeToFile(data) {
-    fs.writeFile('README.md', data, (error)=>{
-        if (error) throw error;// seen this on w3schools fs package examples and loved it. 
-        //The ending wording below for the user letting them know code successfully completed and written file.
-        console.log(`
+  fs.writeFile("README.md", data, (error) => {
+    if (error) throw error; // seen this on w3schools fs package examples and loved it.
+    //The ending wording below for the user letting them know code successfully completed and written file.
+    console.log(`
 
         ----------------------------------------------------------
 
         README file is created and written successfully.
-        Thank you for using the app.`);        
-    })
+        Thank you for using the app.`);
+  });
 }
 // function to initialize program. Everything starts here!
 //It used the array shared by questions.js which includes the array of questions as objects.
 function init() {
-    inquirer.prompt(questions) // question shared by module question.js
+  inquirer
+    .prompt(questions) // question shared by module question.js
     .then(generateMarkdown) // generateMarkdown shared by module generateMarkdown.js uses data from inquirer to create the README template.
     .then(writeToFile); // writes the generated README file to ./README,md
-}// .then in 2 lines above is to wait for the previous task to continue the next one to work in an async way and also to make sure data is ready to proceed. 
-
+} // .then in 2 lines above is to wait for the previous task to continue the next one to work in an async way and also to make sure data is ready to proceed.
 
 // function call to initialize program
 init();
--- utils/questions.js
+++ utils/questions.js
@@ -1,68 +1,67 @@
 // array of questions for user in form of array of objects, it will be exported for inquirer to use and produce response from the user.
 const questions = [
-    {
-        name: 'githubID', 
-        message:"What's github username?"
-    },
+  {
+    name: "githubID",
+    message: "What's github username?",
+  },
 
-    {
-        name: 'githubRepo', 
-        message:"What's the GitHub repo name?"
-    },
+  {
+    name: "githubRepo",
+    message: "What's the GitHub repo name?",
+  },
 
-    {
-        name: 'title', //name part will be assigned as the prop of the object so the answer can  be called as data.title etc.
-        message:"What's the project title?"
-    },
+  {
+    name: "title", //name part will be assigned as the prop of the object so the answer can  be called as data.title etc.
+    message: "What's the project title?",
+  },
 
-    {
-        name: 'description',
-        message:"Please enter the project description?"
-    },
+  {
+    name: "description",
+    message: "Please enter the project description?",
+  },
 
-    {
-        name: 'installation',
-        message:"Please input installation instructions?"
-    },
+  {
+    name: "installation",
+    message: "Please input installation instructions?",
+  },
 
-    {
-        name: 'usage',
-        message:"Please supply information on usage?"
-    },
+  {
+    name: "usage",
+    message: "Please supply information on usage?",
+  },
 
-    {
-        name: 'credits',
-        message:"Please enter any credits you would like to add?"
-    },
+  {
+    name: "credits",
+    message: "Please enter any credits you would like to add?",
+  },
 
-    {
-        name: 'licence',
-        message:"Which licence would you prefer for your project?",
-        type: 'list', //creates a list of options from the choices array below that the user can choose
-        choices: ['Apache2.0', 'GNU3.0','MIT',]
-    },
+  {
+    name: "licence",
+    message: "Which licence would you prefer for your project?",
+    type: "list", //creates a list of options from the choices array below that the user can choose
+    choices: ["Apache2.0", "GNU3.0", "MIT"],
+  },
 
-    {
-        name: 'features',
-        message:"What features does the project have?"
-    },
+  {
+    name: "features",
+    message: "What features does the project have?",
+  },
 
-    {
-        name: 'contribute',
-        message:"Instructions for developers who would like to contribute?"
-    },
+  {
+    name: "contribute",
+    message: "Instructions for developers who would like to contribute?",
+  },
 
-    {
-        name: 'tests',
-        message:"Tests for your project"
-    },
+  {
+    name: "tests",
+    message: "Tests for your project",
+  },
 
-    {
-        name: 'userQuestions',
-        message:"Please enter email address so users can contact you when they have questions?"
-    },
-
-
+  {
+    name: "userQuestions",
+    message:
+      "Please enter email address so users can contact you when they have questions?",
+  },
 ];
 // exporting the array of questions to share with other modules
 module.exports = questions;
